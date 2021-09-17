2e48e78da4f92bf81df37dbeccedc34e1aaa27d0
George Mount <gxm204@case.edu>
2021-09-17 14:35:51 -0400

    update files

--- dev/logistic-regression.R
+++ dev/logistic-regression.R
@@ -5,32 +5,21 @@ library(tidymodels)
 library(tidyverse)
 
 
-# dataset <- read_csv('../data/contestants.csv')
-dataset$mentor <- as_factor(dataset$mentor)
-
-
-set.seed(1234)
-
-
-fitted_logistic_model <- logistic_reg() %>%
-  set_engine('glm') %>%
-  set_mode('classification') %>%
-  fit(mentor ~ style + technique + agility, data = dataset)
-
-
-contestants_predict <- predict(fitted_logistic_model,
-        new_data = dataset,
-        type = 'class') %>% bind_cols(dataset)
+dataset <- read_csv('../data/contestants.csv') %>% 
+  mutate_if(is.numeric, zoo::na.aggregate) %>% 
+  mutate(mentor = as.factor(mentor))
 
 
 
-View(contestants_predict)
+mentor_log_reg <- glm(mentor ~ style + technique + agility, data = dataset, family = 'binomial')
 
 
+dataset <- dataset %>% 
+  bind_cols(mentor_pred = predict(mentor_log_reg, type = 'response')) 
 
 
-grid.table(with(contestants_predict, table(mentor, .pred_class))  )
+dataset <- dataset %>% 
+  mutate(mentor_class = ifelse(mentor_pred > .5, 'yes', 'no'))
 
 
-
-    
+dataset
